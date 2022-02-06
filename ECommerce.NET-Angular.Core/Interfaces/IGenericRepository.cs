9c17a27c2d3257ae3082b08889dc31bc08ca84c5
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-06 17:10:20 +0300

    Generic Repository Oluşturma ve Projeye Tanıtımı

---
+++ ECommerce.NET-Angular.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ECommerce.NET_Angular.Core.DbModels;
+
+namespace ECommerce.NET_Angular.Core.Interfaces
+{
+    // verilen değerler BaseEntityden türemeliler
+    // ve newlenebileen değerler olmalı, interface ya da abstract class veremez.
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+
+    }
+}
