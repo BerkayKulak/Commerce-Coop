85911f362c2d5a775d36623f1307ce6e1b48a2d8
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-06 17:54:19 +0300

    Specification class'ına neden ihtiyaç duyuyoruz ve oluşturulması

---
+++ ECommerce.NET-Angular.Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.NET_Angular.Core.Specifications
+{
+    public class BaseSpecification<T>:ISpecification<T>
+    {
+        public BaseSpecification(Expression<Func<T,bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+        public Expression<Func<T, bool>> Criteria { get; } 
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+
+        // normalde set olmadığı için bir şey ekleyemem ama List türünde Includes
+        protected void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+    }
+}
