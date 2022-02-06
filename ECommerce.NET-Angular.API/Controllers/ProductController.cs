fc52326d8a942332895bdce8ebaf6c0ca0365a53
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-06 15:52:41 +0300

    Add project files.

---
+++ ECommerce.NET-Angular.API/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ECommerce.NET_Angular.Core.DbModels;
+using ECommerce.NET_Angular.Infrastructure.DataContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.NET_Angular.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var data = await _context.Products.ToListAsync();
+            return data;
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProducts(int id)
+        {
+            var data = await _context.Products.FindAsync(id);
+            return data;
+
+        }
+    }
+}
---
+++ ECommerce.NET-Angular.Infrastructure/DataContext/StoreContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.NET_Angular.Core.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.NET_Angular.Infrastructure.DataContext
+{
+    public class StoreContext : DbContext
+    {
+        // options  u DbContextten alır.
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
