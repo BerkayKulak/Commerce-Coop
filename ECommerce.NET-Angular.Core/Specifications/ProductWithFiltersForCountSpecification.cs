ddac97b60978c3f1bd2da5cf60278bd33d058fa1
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-07 00:28:14 +0300

    Search İşlemi Altyapı Oluşturulması

---
+++ ECommerce.NET-Angular.Core/Specifications/ProductWithFiltersForCountSpecification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.NET_Angular.Core.DbModels;
+
+namespace ECommerce.NET_Angular.Core.Specifications
+{
+    public class ProductWithFiltersForCountSpecification:BaseSpecification<Product>
+    {
+        public ProductWithFiltersForCountSpecification(ProductSpecParams productSpecParams):
+            base(x =>
+                (string.IsNullOrWhiteSpace(productSpecParams.Search) || x.Name.ToLower().Contains(productSpecParams.Search))
+                &&
+                (!productSpecParams.BrandId.HasValue || x.ProductBrandId == productSpecParams.BrandId)
+                &&
+                (!productSpecParams.TypeId.HasValue || x.ProductTypeId == productSpecParams.TypeId))
+
+        {
+            
+        }
+    }
+}
