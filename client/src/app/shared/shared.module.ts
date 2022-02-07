4d5863fd879538ef82fa8084a9929513b9618bca
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-07 23:52:28 +0300

    AnaSayfa Slider Ekleme İşlemi

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {CarouselModule} from 'ngx-bootstrap/carousel';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
@@ -12,8 +13,9 @@ import { PagerComponent } from './components/pager/pager.component';
   ],
   imports: [
     CommonModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    CarouselModule.forRoot()
   ],
-  exports:[PaginationModule,PagingHeaderComponent,PagerComponent]
+  exports:[PaginationModule,PagingHeaderComponent,PagerComponent,CarouselModule]
 })
 export class SharedModule { }
