e784981579f75fb0814a5c130bad061e2140e509
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-07 10:33:31 +0300

    UI İşlemleri için Modüllerin Eklemesi - 3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './shared/models/IPagination';
-import { IProduct } from './shared/models/IProduct';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,18 +6,17 @@ import { IProduct } from './shared/models/IProduct';
 })
 export class AppComponent implements OnInit {
   title = 'E-Commerce';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:44379/api/products').subscribe(
-      (response: IPagination) => {
-        this.products = response.data;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+    // this.http.get('https://localhost:44379/api/products').subscribe(
+    //   (response: IPagination) => {
+    //     this.products = response.data;
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   }
+    // );
   }
 }
