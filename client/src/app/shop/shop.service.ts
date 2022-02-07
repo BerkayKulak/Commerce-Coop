3a60eb097d2d2e265f6db17fdd77f37e696e0424
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-07 12:07:17 +0300

    Marka Ve Ürün Tiplerinin Gösterimi

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,17 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IBrand } from '../shared/models/IBrand';
 import { IPagination } from '../shared/models/IPagination';
+import { IProductType } from '../shared/models/IProductType';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShopService {
+  baseUrl = 'https://localhost:44379/api/';
 
-  baseUrl = "https://localhost:44379/api/"
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
+  getProducts() {
+    return this.http.get<IPagination>(this.baseUrl + 'products');
+  }
 
-  getProducts(){
-    return this.http.get<IPagination>(this.baseUrl+"products")
+  getBrands() {
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<IProductType[]>(this.baseUrl + 'products/types');
   }
 }
