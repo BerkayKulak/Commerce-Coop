77c46a7924a5e2672a3ad86cd6172ef6c94d8764
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-07 12:31:20 +0300

    Marka Ve Ürün Tipine Göre Ürünlerin Listelenmesi

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -13,8 +13,8 @@ export class ShopComponent implements OnInit {
   products: IProduct[];
   brands: IBrand[];
   types: IProductType[];
-  brandIdSelected: number;
-  typeIdSelected: number;
+  brandIdSelected = 0; // ilk başta hepsini getirsin diye
+  typeIdSelected = 0;
 
   constructor(private shopService: ShopService) {}
 
