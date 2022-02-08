a29ccdcb75f2d17bce9aaad2243be922ba3d7f0e
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-08 14:45:14 +0300

    Identity Migrations Added

--- ECommerce.NET-Angular.Infrastructure/DataContext/StoreContext.cs
+++ ECommerce.NET-Angular.Infrastructure/DataContext/StoreContext.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ECommerce.NET_Angular.Core.DbModels;
+using ECommerce.NET_Angular.Core.DbModels.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.NET_Angular.Infrastructure.DataContext
 {
-    public class StoreContext : DbContext
+    public class StoreContext : IdentityDbContext<AppUser>
     {
         // options  u DbContextten alır.
         public StoreContext(DbContextOptions<StoreContext> options) : base(options)
@@ -16,6 +18,8 @@ namespace ECommerce.NET_Angular.Infrastructure.DataContext
 
         }
 
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Address> Addresses { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductBrand> ProductBrands { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
