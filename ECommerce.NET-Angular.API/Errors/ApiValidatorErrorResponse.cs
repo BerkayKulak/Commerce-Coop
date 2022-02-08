55cc8f744bbe72f51e6f1aa9d97f5e82576ece8d
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-08 17:17:51 +0300

    Validation İşlemler - 1

---
+++ ECommerce.NET-Angular.API/Errors/ApiValidatorErrorResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ECommerce.NET_Angular.API.Errors
+{
+    public class ApiValidatorErrorResponse:ApiResponse
+    {
+        public ApiValidatorErrorResponse() : base(400)
+        {
+        }
+
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
