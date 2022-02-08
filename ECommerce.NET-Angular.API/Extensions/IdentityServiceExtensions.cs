e87677c046cef3c90d4a50b3a112d698f3b8d0b1
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-08 15:16:47 +0300

    Seed Data/Proje Ayaga Kalkarken Otomatik data ekleme ıslemı

--- ECommerce.NET-Angular.API/Extensions/IdentityServiceExtensions.cs
+++ ECommerce.NET-Angular.API/Extensions/IdentityServiceExtensions.cs
@@ -1,6 +1,25 @@
-﻿namespace ECommerce.NET_Angular.API.Extensions
+﻿using ECommerce.NET_Angular.Core.DbModels.Identity;
+using ECommerce.NET_Angular.Infrastructure.DataContext;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.NET_Angular.API.Extensions
 {
-    public class IdentityServiceExtensions
+    public static class IdentityServiceExtensions
     {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services)
+        {
+            var builder = services.AddIdentityCore<AppUser>();
+
+            builder = new IdentityBuilder(builder.UserType, builder.Services);
+
+            builder.AddEntityFrameworkStores<StoreContext>();
+
+            builder.AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddAuthentication();
+
+            return services;
+        }
     }
 }
