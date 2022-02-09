26a01edaa5f29fc859dbe4ed91292c377b15582c
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-09 15:35:52 +0300

    Ödeme Ekranı Validasyon İşlemleri

--- ECommerce.NET-Angular.API/Dtos/CustomerBasketDto.cs
+++ ECommerce.NET-Angular.API/Dtos/CustomerBasketDto.cs
@@ -9,6 +9,11 @@ namespace ECommerce.NET_Angular.API.Dtos
         [Required]
         public string Id { get; set; }
 
-        public List<BasketItemDto> Items { get; set; } 
+        public List<BasketItemDto> Items { get; set; }
+
+        public int? DeliveryMethodId { get; set; }
+        public string ClientSecret { get; set; }
+        public string PaymentIntentId { get; set; }
+        public decimal ShippingPrice { get; set; }
     }
 }
