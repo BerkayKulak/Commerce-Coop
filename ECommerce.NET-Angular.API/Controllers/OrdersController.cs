9e714b25486db5c5a1a0a007fd4211c1ca8251c7
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-09 10:13:26 +0300

    Ödeme Sayfası tasarımın oluşturulması ve Stepper Eklenmesi

--- ECommerce.NET-Angular.API/Controllers/OrdersController.cs
+++ ECommerce.NET-Angular.API/Controllers/OrdersController.cs
@@ -20,7 +20,7 @@ namespace ECommerce.NET_Angular.API.Controllers
         private readonly IOrderService _orderService;
         private readonly IMapper _mapper;
 
-        public OrdersController(IOrderService orderService,IMapper mapper)
+        public OrdersController(IOrderService orderService, IMapper mapper)
         {
             _orderService = orderService;
             _mapper = mapper;
