2e2f1ad98d5c4a1fee7d89e7d38d9295cac295c7
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-09 09:57:33 +0300

    Sipariş Metotların Duzenlenmesi Ve Mapping Islemlerinin Son Haline Getirilmesi

--- ECommerce.NET-Angular.API/Helpers/MappingProfiles.cs
+++ ECommerce.NET-Angular.API/Helpers/MappingProfiles.cs
@@ -1,7 +1,8 @@
 ﻿using AutoMapper;
 using ECommerce.NET_Angular.API.Dtos;
 using ECommerce.NET_Angular.Core.DbModels;
-using ECommerce.NET_Angular.Core.DbModels.Identity;
+using ECommerce.NET_Angular.Core.DbModels.OrderAggregate;
+using Address = ECommerce.NET_Angular.Core.DbModels.Identity.Address;
 
 namespace ECommerce.NET_Angular.API.Helpers
 {
@@ -18,6 +19,16 @@ namespace ECommerce.NET_Angular.API.Helpers
             CreateMap<CustomerBasketDto, CustomerBasket>();
             CreateMap<BasketItemDto, BasketItem>();
             CreateMap<AddressDto, Core.DbModels.OrderAggregate.Address>().ReverseMap();
+
+            CreateMap<Order, OrderToReturnDto>()
+                .ForMember(d => d.DeliveryMethod, o => o.MapFrom(s => s.DeliveryMethod.ShortName))
+                .ForMember(d => d.ShippingPrice, o => o.MapFrom(s => s.DeliveryMethod.Price));
+
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(d => d.ProductId, o => o.MapFrom(s => s.ItemOrdered.ProductItemId))
+                .ForMember(d => d.ProductName, o => o.MapFrom(s => s.ItemOrdered.ProductName))
+                .ForMember(d => d.ImageUrl, o => o.MapFrom(s => s.ItemOrdered.PictureUrl))
+                .ForMember(d => d.ImageUrl, o => o.MapFrom<OrderItemUrlResolver>());
         }
     }
 }
