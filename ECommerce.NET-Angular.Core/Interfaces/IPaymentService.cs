7a80f390992d399124f476d9a635f0ea39179798
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-09 13:23:41 +0300

    Stipe Entegrasyonu ve Payment Service Yazılması

---
+++ ECommerce.NET-Angular.Core/Interfaces/IPaymentService.cs
@@ -0,0 +1,10 @@
+﻿using ECommerce.NET_Angular.Core.DbModels;
+using System.Threading.Tasks;
+
+namespace ECommerce.NET_Angular.Core.Interfaces
+{
+    public interface IPaymentService
+    {
+        Task<CustomerBasket> CreateOrPaymentIntent(string basketId);
+    }
+}
