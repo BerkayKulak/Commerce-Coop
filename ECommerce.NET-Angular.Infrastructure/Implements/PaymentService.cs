42e11098fcce1b4ffd99ae66291b05570fe295ba
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-02-09 14:32:55 +0300

    Payment Service Method İçerisinin Doldurulması

--- ECommerce.NET-Angular.Infrastructure/Implements/PaymentService.cs
+++ ECommerce.NET-Angular.Infrastructure/Implements/PaymentService.cs
@@ -4,15 +4,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ECommerce.NET_Angular.Core.DbModels;
+using ECommerce.NET_Angular.Core.DbModels.OrderAggregate;
 using ECommerce.NET_Angular.Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Stripe;
+using Product = ECommerce.NET_Angular.Core.DbModels.Product;
 
 namespace ECommerce.NET_Angular.Infrastructure.Implements
 {
     public class PaymentService:IPaymentService
     {
-        public Task<CustomerBasket> CreateOrPaymentIntent(string basketId)
+        private readonly IBasketRepository _basketRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IConfiguration _configuration;
+
+        public PaymentService(IBasketRepository basketRepository, IUnitOfWork unitOfWork,IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _unitOfWork = unitOfWork;
+            _configuration = configuration;
+            _basketRepository= basketRepository;
+        }
+        public async Task<CustomerBasket> CreateOrPaymentIntent(string basketId)
+        {
+            StripeConfiguration.ApiKey = _configuration["StripeSettings:SecretKey"];
+
+            var basket = await _basketRepository.GetBasketAsync(basketId);
+
+            var shippingPrice = 0m;
+
+            if (basket.DeliveryMethodId.HasValue)
+            {
+                var deliveryMethod = await _unitOfWork.Repository<DeliveryMethod>().GetByIdAsync((int)basket.DeliveryMethodId);
+
+                shippingPrice = deliveryMethod.Price;
+            }
+
+            foreach (var item in basket.Items)
+            {
+                var productItem = await _unitOfWork.Repository<Product>().GetByIdAsync(item.Id);
+
+                if (item.Price != productItem.Price)
+                {
+                    item.Price = productItem.Price;
+                }
+            }
+
+            var service = new PaymentIntentService();
+
+            PaymentIntent intent;
+
+            if (string.IsNullOrEmpty(basket.PaymentIntentId))
+            {
+                var options = new PaymentIntentCreateOptions()
+                {
+                    Amount = (long) basket.Items.Sum(i => i.Quantity * (i.Price * 100)) + (long) shippingPrice * 100,
+                    Currency = "usd",
+                    PaymentMethodTypes = new List<string>(){"card"}
+                };
+
+                intent = await service.CreateAsync(options);
+
+                basket.PaymentIntentId = intent.Id;
+
+                basket.ClientSecret = intent.ClientSecret;
+
+            }
+            else
+            {
+                var options = new PaymentIntentUpdateOptions()
+                {
+                    Amount = (long)basket.Items.Sum(i => i.Quantity * (i.Price * 100)) + (long)shippingPrice * 100,
+                    
+                };
+
+                await service.UpdateAsync(basket.PaymentIntentId, options);
+            }
+
+            await _basketRepository.UpdateBasketAsync(basket);
+
+            return basket;
         }
     }
 }
